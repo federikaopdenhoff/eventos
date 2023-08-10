4d6995f9b3de32115fdfc1e032bcb536342ec062
federikaopdenhoff <84040507+federikaopdenhoff@users.noreply.github.com>
2023-08-10 20:13:02 -0300

    solucion click added in new branch

--- eventos.js
+++ eventos.js
@@ -4,3 +4,10 @@ const divElement = document.getElementById('container');
 divElement.addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+const button = document.getElementById('saludarBtn');
+
+button.addEventListener('click', function(event) {
+    alert('Hola!');
+    event.stopPropagation();
+});
